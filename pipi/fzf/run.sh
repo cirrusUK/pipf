80e630f2384093f6d22b2bb878292401633ac8d7
samedamci <samedamci@disroot.org>
2021-08-24 23:54:36 +0200

    Add -U option for pip

--- pipi/fzf/run.sh
+++ pipi/fzf/run.sh
@@ -3,4 +3,4 @@
 python3 -m pipi.fzf.packages_list |
     fzf --multi --cycle --height 45% --preview-window right,70% \
         --preview 'python3 -m pipi.fzf.preview {1}' |
-    pip install --user -r /dev/stdin --disable-pip-version-check --no-python-version-warning
+    pip install --user -U -r /dev/stdin --disable-pip-version-check --no-python-version-warning
